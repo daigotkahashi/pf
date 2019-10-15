dbc45a4f4a852a57259c932cb6fbc70a8c51aa7e
daigon <pillow813.fuzzy69002@gmail.com>
2019-10-15 13:07:08 +0900

    update: vue.jsをいじってみたので追加

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+var test = new Vue({
+  el: '#test',
+  data: {
+    title: 'vueのtest',
+    firstName: '大瑚',
+    lastName: '高橋',
+    image: [
+    'img/ダウンロード.jpeg',
+    'img/ダウンロード (1).jpeg',
+    'img/ダウンロード (2).jpeg'
+    ]
+  }
+})
